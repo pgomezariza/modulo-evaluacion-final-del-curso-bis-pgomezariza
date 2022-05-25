e6680d14c40a9ef123b4d5007ad33f9427a030c9
Paula Gomez Ariza <pgomezariza@gmail.com>
2022-05-25 17:29:09 +0200

    add recipe not found message

--- src/components/ListRecipe.js
+++ src/components/ListRecipe.js
@@ -2,6 +2,14 @@ import ItemRecipe from "./ItemRecipe";
 
 
 function ListRecipe(props) { 
+  //Si el array está vacio, devuelvo html con mensaje.
+  if(props.recipes.length < 1){
+    return (
+      <p className="recipeNotFound">Lo sentimos, no hay ninguna receta que coincida con su busqueda.</p>
+    )
+  }
+  
+  //Si no está vacio, continuo.
   const recipesElements = props.recipes.map((recipe) => {
     return (
       <li key={recipe.id}> 
