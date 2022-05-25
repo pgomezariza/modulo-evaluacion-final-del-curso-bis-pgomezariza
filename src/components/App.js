f30f8c35a401b32a97b660c03148268866d4c51c
Paula Gomez Ariza <pgomezariza@gmail.com>
2022-05-25 11:52:06 +0200

    save the data to an array

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,37 @@
 import { useState, useEffect } from "react";
 import getApiData from "../services/recipesApi";
+
+function App() {
+  //Variables de Estado
+  const [dataRecipes, setDataRecipes] = useState([]);
+
+
+  useEffect(() => {
+    getApiData().then((dataFromApi) => {
+      console.log(dataFromApi);
+      setDataRecipes(dataFromApi);
+    });
+  }, []);
+
+
+  return (
+    <>
+      <h1 className="title">Las Recetas de Paula</h1>
+      </>
+  );
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default App;
