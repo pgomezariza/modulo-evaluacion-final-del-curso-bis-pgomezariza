7517ff147c06deaf175635edf70f97d0e54389df
Paula Gomez Ariza <pgomezariza@gmail.com>
2022-05-25 13:43:43 +0200

    return in ItemRecipe each of the contents that the li will have

--- src/components/ItemRecipe.js
+++ src/components/ItemRecipe.js
@@ -1,4 +1,14 @@
-function ItemRecipe (){
+import { Link } from "react-router-dom";
 
+function ItemRecipe (){
+  return (
+    <>
+      <img/>
+      <h2></h2>
+      <p></p>
+      <Link></Link>
+
+    </>
+  );
 }
 export default ItemRecipe; 
