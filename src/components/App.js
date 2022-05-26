7c0c388ae1e7855c9f5fbca2f7dfda7ab1c4d1ec
Paula Gomez Ariza <pgomezariza@gmail.com>
2022-05-26 08:07:35 +0200

    add filter by type of food

--- src/components/App.js
+++ src/components/App.js
@@ -18,6 +18,10 @@ function App() {
   return (
     <>
       <h1 className="title">Las Recetas de Paula</h1>
+      <p className="quote_1">Rico Rico y con Fundamento...</p>
+      <p className="quote_by">By Karlos Arguiñano</p>
+      <p className="quote_2">Como a tripa vacia CORAZON sin alegría no puede ser...a continuación encontrarás unas recetas ricas ricas para tus menus semanales.</p>
+      <p className="quote_3">Bon Appétit!</p>
       <ListRecipe recipes={dataRecipes}/>
       </>
   );
