fff4b9c350e91a7f2121d1bade6c30314b991339
Paula Gomez Ariza <pgomezariza@gmail.com>
2022-05-26 08:49:01 +0200

    add text input to filter by food type

---
+++ src/components/FilterTypeFood.js
@@ -0,0 +1,20 @@
+function FilterTypeFood() {
+  return (
+    <>
+      <div>
+        <label>
+          Recetas
+          <input
+            type="text"
+            placeholder="Buscar por tipo de comida..."
+            className=""
+            name=""
+          />
+        </label>
+      </div>
+
+    </>
+  );
+
+} 
+export default FilterTypeFood;
