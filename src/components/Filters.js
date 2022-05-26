87c22277de016925b594c7e3b5e2129a9faeae16
Paula Gomez Ariza <pgomezariza@gmail.com>
2022-05-26 08:31:18 +0200

    import FilterTypeFood

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,4 +1,11 @@
+import FilterTypeFood from './FilterTypeFood';
+
 function Filters() {
+  return <section>
+    <form>
+      <FilterTypeFood />
+    </form>
+  </section>
 
 }
 export default Filters; 
