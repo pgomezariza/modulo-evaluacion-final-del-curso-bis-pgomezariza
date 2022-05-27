ad61f5d51c1f529a7a6d88fff4d3da300d119c4d
Paula Gomez Ariza <pgomezariza@gmail.com>
2022-05-27 21:33:55 +0200

    add styles at ListRecipe

--- src/components/ListRecipe.js
+++ src/components/ListRecipe.js
@@ -6,22 +6,22 @@ function ListRecipe(props) {
   //Si el array está vacio, devuelvo html con mensaje.
   if(props.recipes.length < 1){
     return (
-      <p className="recipeNotFound">Lo sentimos, no hay ninguna receta que coincida con su busqueda.</p>
+      <p className="recipeNotFound">¡OPS! No hemos encontrado una receta que coincida con su busqueda.</p>
     )
   }
   
   //Si no está vacio, continuo.
   const recipesElements = props.recipes.map((recipe) => {
     return (
-      <li> 
+      <li className="recipes"> 
         <ItemRecipe recipe={recipe} />
       </li>
     );
   });
 
   return (
-    <section className="">
-      <ul className="">{recipesElements}</ul>
+    <section>
+      <ul className="recipes__all">{recipesElements}</ul>
     </section>
   );
 };
